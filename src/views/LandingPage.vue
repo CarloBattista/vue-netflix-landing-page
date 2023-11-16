0a1498ddfb5d02603e0bfd9dcc8fa2b441b54e4c
Cicidong00 <136182931+Cicidong00@users.noreply.github.com>
2023-11-16 19:08:14 +0100

    banner ads completed

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,18 +2,21 @@
     <HeaderComp />
     <div class="mainView">
         <HeroComp />
+        <BannerComp />
     </div>
 </template>
 
 <script>
 import HeaderComp from '../components/HeaderComp.vue';
 import HeroComp from '../components/HeroComp.vue';
+import BannerComp from '../components/BannerComp.vue';
 
 export default {
     name: "LandingPage",
     components: {
         HeaderComp,
-        HeroComp
+        HeroComp,
+        BannerComp
     },
 }
 </script>
